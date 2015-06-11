0890aa49a3b921a059bc2c8733dd9cfb12069aa5
pxlxinzhao <pxlxinzhao@gmail.com>
2015-06-11 18:12:18 -0400

    blog

--- WebApp/main.js
+++ WebApp/main.js
@@ -2,12 +2,17 @@ var express = require('express');
 var app = express();
 var path = require("path");
 
-app.use(express.static(__dirname + "/View"));
+// app.use(express.static(__dirname + "/View"));
 // app.use(express.static(__dirname + "/Script"));
 
 app.get('/', function(req, res){
 	// res.json({message:'Hello World'});
-	res.sendFile('index.html');
+	res.sendFile(__dirname + '/View/index.html');
+});
+
+app.get('/user', function(req, res){
+	// res.json({message:'Hello World'});
+	res.sendFile(__dirname + '/View/user.html');
 });
 
 app.listen(process.env.PORT || 7000);
