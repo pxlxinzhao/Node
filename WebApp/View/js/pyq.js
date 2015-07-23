5ef9a9ef68e12637a4ddeaacd02cf95be4bd3575
Patrick_Pu <pxlxinzhao@gmail.com>
2015-07-22 21:38:51 -0400

    alert

--- WebApp/View/js/pyq.js
+++ WebApp/View/js/pyq.js
@@ -18,10 +18,41 @@ $(function(){
                             '<input class="modal-input" type="text" name="email" placeholder="邮箱" />' +
                         '</form>';
 
-    //login prompt
-	$("#loginBtn").click(function(){
+    var topicMessage = 
+                       '<form id="topicForm" class="modal-input">' +
+                            '<input style="margin-bottom: 10px;" class="modal-input" type="text" name="username" placeholder="用户名" />' +
+                            '<input style="margin-bottom: 10px;" class="modal-input" type="password" name="password" placeholder="密码" />' +
+                            '<input style="margin-bottom: 10px;" class="modal-input" type="password" name="password2" placeholder="确认密码" />' +
+                            '<input class="modal-input" type="text" name="email" placeholder="邮箱" />' +
+                        '</form>';
 
-		 BootstrapDialog.show({
+    function addTopic(){
+        var boxHtml = 
+                    '<div id="yoyo" class="topicBox col-lg-3 col-sm-4 col-xs-6" style="background-color: '+ 
+                            randColor() +'; min-height:150px;">' + 
+                        // '<a href="#">' + 
+                        // '</a>' +
+                    '</div>';
+
+        console.log('Add topic', randColor());
+        $portfolioSection.find('.row').append(boxHtml);
+        // $portfolioSection.empty();
+    }
+
+    function myAlert(message){
+        BootstrapDialog.show({
+            title: '提示',
+            size: BootstrapDialog.SIZE_SMALL,
+            message: message
+        });
+    }
+
+    function createTopicForm(){
+
+    }
+
+    function createLoginRegisterForm(){
+         BootstrapDialog.show({
             title: '用户登录',
             onshow: function(dialog) {
                 dialog.setSize(BootstrapDialog.SIZE_SMALL);
@@ -44,7 +75,7 @@ $(function(){
 
                         for (var i=0; i<formArray.length; i++){
                             if (!formArray[i] || formArray[i].value.length < 1){
-                                alert('Plz fill all fields');
+                                myAlert('Plz fill all fields');
                                 return;
                             }
                         }
@@ -58,7 +89,7 @@ $(function(){
 
                         }).fail(function(jqXHR, textStatus, error){
                              console.log('error: ' + error);
-                             alert('failed to login');
+                             myAlert('failed to login');
                         });                    }
 
                 }
@@ -78,7 +109,7 @@ $(function(){
                         //--form check 
                         for (var i=0; i<formArray.length; i++){
                             if (!formArray[i] || formArray[i].value.length < 1){
-                                alert('Plz fill all fields');
+                                myAlert('Plz fill all fields');
                                 return;
                             }
                         }
@@ -91,20 +122,20 @@ $(function(){
                             dialog.state = "login";
                             dialog.setTitle('用户登录');
                             dialog.setMessage(loginMessage);
-                            alert('Registration success, plz login');
+                            myAlert('Registration success, plz login');
                         }).fail(function(jqXHR, textStatus, error){
                              console.log('error: ' + error);
-                             alert('failed to register');
+                             myAlert('failed to register');
                         });
                         
                     }
                 }
             }]
         });
+    }
 
-		 // $('.modal-content .modal-header').hide();
-
-	});
+    //login prompt
+    $("#loginBtn").click(createLoginRegisterForm);
 
     //add topic
     var $addTopic = $('#createTopicBtn');
@@ -114,20 +145,6 @@ $(function(){
         event.preventDefault();
         addTopic();
     });
-
-    function addTopic(){
-        var boxHtml = 
-                    '<div id="yoyo" class="topicBox col-lg-3 col-sm-4 col-xs-6" style="background-color: '+ 
-                            randColor() +'; min-height:150px;">' + 
-                        // '<a href="#">' + 
-                        // '</a>' +
-                    '</div>';
-
-        console.log('Add topic', randColor());
-        $portfolioSection.find('.row').append(boxHtml);
-        // $portfolioSection.empty();
-    }
-
 })
 
 
